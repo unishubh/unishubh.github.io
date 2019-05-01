e139108e3f8cae3b7fcb46c594f00e22cb8e9d2e
Shubh Shukla <unishubh1@gmail.com>
2019-05-01 16:22:00 +0530

    Pie Chart Added

--- sip.js
+++ sip.js
@@ -1,6 +1,6 @@
 
 
-let calculateSip =  (principle, number_per_year, interest, time_in_years) => {
+let calculateSip = (principle, number_per_year, interest, time_in_years) => {
     let actual_rate = interest / number_per_year / 100;
     let installments = number_per_year * time_in_years;
     calc = principle * Math.pow((1 + actual_rate / number_per_year), number_per_year * time_in_years);
@@ -22,17 +22,17 @@ let fetchData = async () => {
 
 
 let drawBarGraph = async (calculated_values) => {
-    var densityCanvas = document.getElementById("barChart");
-    var densityData = {
-        label: 'Expected Value (in Thousand Rs.)',
-        backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850", "#3e95cd", "#8e5ea2"],
+    let densityCanvas = document.getElementById("barChart");
+    let densityData = {
+        label: 'Expected Value',
+        backgroundColor: '#ccddee',//["#ccddee", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850", "#3e95cd", "#8e5ea2"],
         data: calculated_values
     };
 
     var barChart = new Chart(densityCanvas, {
         type: 'bar',
         data: {
-            labels:[5,10,15,20,25,30],
+            labels: [5, 10, 15, 20, 25, 30],
             datasets: [densityData]
         },
         options: {
@@ -44,17 +44,79 @@ let drawBarGraph = async (calculated_values) => {
 
                 }]
             },
+
+            plugins: {
+                datalabels: {
+                    color: 'black',
+                    align: 'end',
+                    display: function (context) {
+                        console.log("Algo: " + context);
+                        return context.dataset.data[context.dataIndex] > 15;
+                    },
+                    font: {
+                        weight: 'bold'
+                    },
+                    formatter: function (value, context) {
+                        if (value >= 1000 && value < 100000) {
+                            return Math.round((value / 1000) * 100) / 100 + "Th"
+                        }
+                        else if (value >= 100000 && value < 10000000) {
+                            return Math.round((value / 100000) * 100) / 100 + "L"
+                        }
+                        else if (value > 10000000) {
+                            return Math.round((value / 10000000) * 100) / 100 + "Cr"
+                        }
+                    }
+                }
+            }
         }
 
     });
 
 };
 
-let putData = async (principle, years, result) => {
-    let net_invested = principle*12*years;
-    let net_value = result;
-    let net_gain = result-net_invested;
+let drawPieChart = async (expected, gained) => {
+    let data = {
+        datasets: [{
+            data: [expected, gained],
+            backgroundColor: ["#DEB887", "#A9A9A9"]
+        }],
+        labels: ['Invested', 'Gained']
+    };
 
+    let options = {
+        responsive: true,
+        title: {
+            display: true,
+            position: "top",
+            text: "Pie Chart",
+            fontSize: 18,
+            fontColor: "#111"
+        },
+        legend: {
+            display: true,
+            position: "bottom",
+            labels: {
+                fontColor: "#333",
+                fontSize: 16
+            }
+        }
+    };
+
+    let ctx = document.getElementById("pieChart");
+    var myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: data,
+
+    });
+
+}
+
+let putData = async (principle, years, result) => {
+    let net_invested = principle * 12 * years;
+    let net_value = result;
+    let net_gain = result - net_invested;
+    await drawPieChart(Math.round(net_invested), Math.round(net_gain));
     document.getElementById('expected').innerHTML = Math.round(net_value);
     document.getElementById('invested').innerHTML = Math.round(net_invested);
     document.getElementById('gained').innerHTML = Math.round(net_gain);
@@ -64,12 +126,13 @@ let getResults = async () => {
     let data = await fetchData();
     console.log(data);
     let result = await calculateSip(data.principle, 12, data.rate, data.years);
-    let calculation_periods = [5,10,15,20,25,30];
-    let calculated_values = calculation_periods.map(time =>  calculateSip(data.principle, 12, data.rate,time)/1000);
+    let calculation_periods = [5, 10, 15, 20, 25, 30];
+    let calculated_values = calculation_periods.map(time => Math.round(calculateSip(data.principle, 12, data.rate, time)));
     console.log(result);
     console.log(calculated_values);
     await drawBarGraph(calculated_values);
     await putData(data.principle, data.years, result);
+    document.getElementById('result').style.display = "block";
 };
 
 //window.onload = drawBarGraph;
