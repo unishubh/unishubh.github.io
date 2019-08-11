b9b3bac4d55fde09120192f21117153b1a4f9944
Shubham Shukla <shubham.shukla@ITIN000220-MAC.local>
2019-08-11 13:05:56 +0530

    number in words

--- sip.js
+++ sip.js
@@ -1,4 +1,21 @@
+var a = ['','One ','Two ','Three ','Four ', 'Five ','Six ','Seven ','Eight ','Nine ','Ten ','Eleven ','Twelve ','Thirteen ','Fourteen ','Fifteen ','Sixteen ','Seventeen ','Eighteen ','Nineteen '];
+var b = ['', '', 'Twenty','Thirty','Forty','Fifty', 'Sixty','Seventy','Eighty','Ninety'];
 
+function inWords (num) {
+    if ((num = num.toString()).length > 9) return 'More than 10 Crore';
+    n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    if (!n) return; var str = '';
+    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'crore ' : '';
+    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'lakh ' : '';
+    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+    str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'only ' : '';
+    return str;
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 let calculateSip = (principle, number_per_year, interest, time_in_years) => {
     let actual_rate = interest / number_per_year / 100;
@@ -142,9 +159,9 @@ let putData = async (principle, years, result) => {
     let net_value = result;
     let net_gain = result - net_invested;
     await drawPieChart(Math.round(net_invested), Math.round(net_gain));
-    document.getElementById('expected').innerHTML = "Rs." + Math.round(net_value);
-    document.getElementById('invested').innerHTML = "Rs." + Math.round(net_invested);
-    document.getElementById('gained').innerHTML = "Rs." + Math.round(net_gain);
+    document.getElementById('expected').innerHTML = "Rs. " + numberWithCommas(Math.round(net_value));
+    document.getElementById('invested').innerHTML = "Rs. " + numberWithCommas(Math.round(net_invested));
+    document.getElementById('gained').innerHTML = "Rs. " + numberWithCommas(Math.round(net_gain));
 }
 
 let getResults = async () => {
@@ -154,24 +171,16 @@ let getResults = async () => {
     let calculation_periods = [5, 10, 15, 20, 25, 30];
     let calculated_values = calculation_periods.map(time => Math.round(calculateSip(data.principle, 12, data.rate, time)));
     console.log(result);
+    let words=inWords(Math.round(result));
+
     console.log(calculated_values);
     await drawBarGraph(calculated_values);
     await putData(data.principle, data.years, result);
+    document.getElementById('inwords').innerHTML="Total Corpus : "+words
     document.getElementById('result').style.display = "block";
     document.getElementById('barBanner').innerHTML = "Predictions based on investment of Rs. "+data.principle+" at "+data.rate+"% interest";
 };
 
-formatter= async (value) => {
-    if (value >= 1000 && value < 100000) {
-        return Math.round((value / 1000) * 100) / 100 + "Th"
-    }
-    else if (value >= 100000 && value < 10000000) {
-        return Math.round((value / 100000) * 100) / 100 + "L"
-    }
-    else if (value > 10000000) {
-        return Math.round((value / 10000000) * 100) / 100 + "Cr"
-    }
-};
 
 //window.onload = drawBarGraph;
 
